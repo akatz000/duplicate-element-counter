3c24db2d7758116796f61182c471f6e0738017db
Ariel Katz <captainkatz@gmail.com>
2015-12-23 20:43:51 -0500

    add tests

---
+++ code_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require_relative 'code.rb'
+
+RSpec.describe "duplicate_counter" do
+  it "passes examples from the readme" do
+    expect(duplicate_counter([1,7,7,7,3,5])).to eq(2)
+    expect(duplicate_counter[5,7,9]).to eq(0)
+    expect(duplicate_counter[0,-5,-5,33,33,33]).to eq(3)
+  end
+
+  it "handles empty array properly" do
+    expect(duplicate_counter([])).to eq(0)
+  end
+end
