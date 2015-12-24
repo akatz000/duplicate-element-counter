86138fba656e72e3b4daf02c2a4bca180ef236cb
Ariel Katz <captainkatz@gmail.com>
2015-12-23 21:40:38 -0500

    add corner case test. write methods to pass tests with efficiency in mind

--- code.rb
+++ code.rb
@@ -1,1 +1,21 @@
-#YOUR CODE GOES HERE
+def duplicate_counter(array)
+  element_count = count_elements(array)
+  num_duplicates = 0
+  element_count.values.each do |count|
+    num_duplicates += (count - 1)
+  end
+  num_duplicates
+end
+
+def count_elements(array)
+  element_count = {}
+  array.each do |element|
+    key = "#{element}#{element.class}"
+    if element_count[key]
+      element_count[key] += 1
+    else
+      element_count[key] = 1
+    end
+  end
+  element_count
+end
--- code_spec.rb
+++ code_spec.rb
@@ -4,11 +4,15 @@ require_relative 'code.rb'
 RSpec.describe "duplicate_counter" do
   it "passes examples from the readme" do
     expect(duplicate_counter([1,7,7,7,3,5])).to eq(2)
-    expect(duplicate_counter[5,7,9]).to eq(0)
-    expect(duplicate_counter[0,-5,-5,33,33,33]).to eq(3)
+    expect(duplicate_counter([5,7,9])).to eq(0)
+    expect(duplicate_counter([0,-5,-5,33,33,33])).to eq(3)
   end
 
   it "handles empty array properly" do
     expect(duplicate_counter([])).to eq(0)
+  end
+
+  it "handles numbers and number strings properly" do
+    expect(duplicate_counter(["10",10,11,"11"])).to eq(0)
   end
 end
